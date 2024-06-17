127a84611fb652bda849b6e6bfdf8dd6cdd6a823
Ismarianto Syafril <ismariantos@mncsekuritas.local>
2024-06-18 02:16:04 +0700

    adsad

--- package/Models/Product.ts
+++ package/Models/Product.ts
@@ -1,5 +1,13 @@
 export class Product {
-    constructor(params: any) {
+    public name;
+    public price;
 
+    constructor(params: { [key: string]: any }) {
+        this.entity = { ...params };
+        this.name = params.name;
+        this.price = params.price;
     }
+
+    public tablename: string = "product"; // Set the table name as a string constant
+    public entity: { [key: string]: any } = {}; // Initialize entity as an empty object
 }
